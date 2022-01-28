b54825341ad738d22c95969f50be9d3d968b031b
Antoniok2 <antoniopiazza18@gmail.com>
2022-01-28 16:37:59 +0100

    create model, factory and seed

---
+++ Laravel_CRUD/database/factories/ComicFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Comic;
+use Faker\Generator as Faker;
+
+$factory->define(Comic::class, function (Faker $faker) {
+    return [
+
+        'title' => $faker -> word(),
+        'author' => $faker -> name(),
+        'release_date' => $faker -> date(),
+        'count' => $faker -> numberBetween(0, 500),
+
+    ];
+});
